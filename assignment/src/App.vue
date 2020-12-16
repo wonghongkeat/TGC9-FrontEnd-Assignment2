31f191f9390abb9d212f898ce4da936f1a4ca281
wonghongkeat <hongkeat1991@gmail.com>
2020-12-16 10:01:09 +0000

    edited:
    edited vue template

---
+++ assignment/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <FrontPage/>
+  </div>
+</template>
+
+<script>
+
+import FrontPage from "./components/FrontPage"
+
+export default {
+  name: 'App',
+  components: {
+      FrontPage
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ assignment/src/components/FrontPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Shoot The Ducks!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1{
+    text-align: center;
+    font-weight: bold;
+}
+</style>
+
