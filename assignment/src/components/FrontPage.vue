ce3a131c185a99792095ff13d86f90fb47ea583f
wonghongkeat <hongkeat1991@gmail.com>
2020-12-16 10:08:12 +0000

    edited:
    link api with vue

--- assignment/src/components/FrontPage.vue
+++ assignment/src/components/FrontPage.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
       <h1>Shoot The Ducks!</h1>
+  <div v-for='f in faults' v-bind:key='f._id'>
+      <ul>
+          <li>name: {{f.name}}</li>
+          <li>score: {{f.score}}</li>
+      </ul>
+  </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+    data: function(){
+        return{
+            'faults':[]
+        }
+    },
+
+    created:async function(){
+        let response = await axios.get('https://3000-dfcbe04c-de1f-4c92-97a7-ec5d4aa86552.ws-us03.gitpod.io/');
+        this.faults = response.data;
+    },
 
 }
 </script>
