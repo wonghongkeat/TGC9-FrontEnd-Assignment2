7930652f869caedaf63e66273405c5a45975443f
wonghongkeat <hongkeat1991@gmail.com>
2020-12-17 02:24:38 +0000

    edited:
    edited fonts and able to pull from API

--- assignment/src/components/FrontPage.vue
+++ assignment/src/components/FrontPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <h1>Shoot The Ducks!</h1>
-  <div v-for='f in faults' v-bind:key='f._id'>
+  <div v-for='f in scores' v-bind:key='f._id'>
       <ul>
           <li>name: {{f.name}}</li>
           <li>score: {{f.score}}</li>
@@ -14,24 +14,34 @@
 import axios from 'axios';
 
 export default {
+      created:async function(){
+        let response = await axios.get('https://3000-dfcbe04c-de1f-4c92-97a7-ec5d4aa86552.ws-us03.gitpod.io/');
+        this.scores = response.data;
+    },
+
+
     data: function(){
         return{
-            'faults':[]
+            'scores':[]
         }
     },
 
-    created:async function(){
-        let response = await axios.get('https://3000-dfcbe04c-de1f-4c92-97a7-ec5d4aa86552.ws-us03.gitpod.io/');
-        this.faults = response.data;
-    },
+  
 
 }
+
+
+
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+
 h1{
     text-align: center;
     font-weight: bold;
+    font-family: 'VT323', monospace;
+    font-size: 80px;
 }
 </style>
 
