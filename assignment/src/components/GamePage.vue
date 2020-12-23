a60c1b2c9410182d176f765b78cd5f33fb30feca
wonghongkeat <hongkeat1991@gmail.com>
2020-12-23 07:06:36 +0000

    edited:
    added timer countdown function

--- assignment/src/components/GamePage.vue
+++ assignment/src/components/GamePage.vue
@@ -4,11 +4,18 @@
     <p>{{ randomNumber }}</p>
     <p>{{ randomNumber2 }}</p>
     <p>points:{{ points }}</p>
+    <button v-on:click="countDown" class="button">
+      {{ timer }}
+    </button>
     <table>
       <tr v-for="(r, row) in board" v-bind:key="row">
         <td v-for="(c, column) in r" v-bind:key="row * 3 + column">
-         <!-- <h1 v-on:click='toDelete'>{{c}}</h1> -->
-          <img src='./duck.png' v-on:click='toDelete' v-show='board[row][column]'/>
+          <!-- <h1 v-on:click='toDelete'>{{c}}</h1> -->
+          <img
+            src="./duck.png"
+            v-on:click="toDelete"
+            v-show="board[row][column]"
+          />
         </td>
       </tr>
     </table>
@@ -19,6 +26,7 @@
 export default {
   data: function () {
     return {
+      timer : 0,
       board: [
         ["", "", "", "", ""],
         ["", "", "", "", ""],
@@ -28,31 +36,39 @@ export default {
       randomNumber: 0,
       randomNumber2: 0,
       points: 0,
-      display: true
+
+ 
     };
   },
   methods: {
+    countDown: function () {
+   
+      if (this.timer < 10) {
+        setTimeout(() => {
+          this.timer += 1;
+          this.countDown();
+        }, 1000);
+      }
+    
+    },
     randomStart: function () {
       let Xrow = parseInt(Math.random() * (3 - 0) + 0);
       this.randomNumber = Xrow;
       let Xcolumn = parseInt(Math.random() * (5 - 0) + 0);
       this.randomNumber2 = Xcolumn;
       this.board[Xrow][Xcolumn] = true;
-      if(this.board[Xrow][Xcolumn] === true){
-       this.board[Xrow][Xcolumn] = true;
-      }
     },
     toDelete: function () {
       for (let r = 0; r < this.board.length; r++) {
         for (let c = 0; c < 5; c++) {
-          if (this.board[r][c]===true) { 
+          if (this.board[r][c] === true) {
             this.$set(this.board[r], c, "");
-            break
+            break;
           }
         }
       }
       this.randomStart();
-      this.points += 1
+      this.points += 1;
     },
   },
 };
@@ -71,5 +87,12 @@ td {
 img {
   width: 100px;
   height: 100px;
+}
+
+.button{
+    width: 100px;
+    height:50px;
+    font-weight: bold;
+    font-size: 2rem;
 }
 </style>
